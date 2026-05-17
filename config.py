75d6ccbe000aeef5bc15af96db10b6116f9af640
Oybekjon1928 <oybekmadaminov923@gmail.com>
2026-05-17 22:08:16 +0500

    update channel link to t.me/Markentii

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ ADMIN_USERNAME: str = os.getenv("ADMIN_USERNAME", "admin")
 
 PORTFOLIO_DIR = os.path.join(os.path.dirname(__file__), "portfolio")
 DATABASE_URL: str = os.getenv("DATABASE_URL", "")
-CHANNEL_URL: str = os.getenv("CHANNEL_URL", "https://t.me/Power_BI1")
+CHANNEL_URL: str = os.getenv("CHANNEL_URL", "https://t.me/Markentii")
 
 SHEETS_CREDS_FILE: str = os.getenv("SHEETS_CREDS_FILE", "credentials.json")
 SHEETS_ID: str = os.getenv("SHEETS_ID", "")
